d37da7eae0835d3091b46c3036db78b092e7817c
Felipe <felipevalentim@live.com>
2019-11-12 01:58:20 -0300

    Construção dos dois primeiros algoritmos de Java01

--- src/com/db1/db1start/App.java
+++ src/com/db1/db1start/App.java
@@ -1,7 +1,33 @@
 package com.db1.db1start;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[]args){
-        System.out.println("Hello World");
+
+        // * Exercício  de informar o número menor * \\
+        //canner ler = new Scanner(System.in);
+        //System.out.println("Digite um número:");
+        //Integer valor1 = ler.nextInt();
+        //System.out.println("Digite um número:");
+        //Integer valor2 = ler.nextInt();
+        //if(valor1 > valor2){
+        //System.out.println("Valor 1 maior que valor 2");
+        //System.out.println("Soma dos dois valores: " + (valor1+valor2));
+        //}else{
+        //System.out.println("Valor 1 menor que valor 2");
+        //System.out.println("Subtração dos dois valores: " + (valor1-valor2));}
+
+        // * Exercício  de alterar o tamanho das letras * \\
+        //Scanner ler = new Scanner(System.in);
+        //System.out.println("Informe um nome:");
+        //String textoNormal = ler.nextLine();
+        //String maisculo =textoNormal.toUpperCase();
+        //String minusculo =textoNormal.toLowerCase();
+        //System.out.println("Texto normal: " + textoNormal);
+        //System.out.println("Texto Maiusculo: " + maisculo);
+        //System.out.println("Texto Minusculo: " + minusculo);
+
     }
 }
